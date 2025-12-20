0f16ec2a351384cf3b6d6b6ea79e36ab53e4ea13
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-20 23:51:32 +0100

    add workflow and workflow_steps serializer

--- workflows/serializers.py
+++ workflows/serializers.py
@@ -7,3 +7,10 @@ class workflow_serializer(serializers.ModelSerializer) :
         model = WorkFlow
         fields = "__all__"
         read_only_fields = ['created_by']
+
+
+class workflow_steps_serializer(serializers.ModelSerializer) :
+    class Meta :
+        model = WorkFlowStep
+        fields = "__all__"
+        read_only_fields = ['workflow']
