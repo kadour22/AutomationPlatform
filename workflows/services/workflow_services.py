412cc29044bd17c4f3cbe0418040839928d6d81b
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-20 23:57:19 +0100

    add workflow and workflow_retireve services

--- workflows/services/workflow_services.py
+++ workflows/services/workflow_services.py
@@ -2,11 +2,16 @@ from workflows.models import WorkFlow
 from workflows.serializers import workflow_serializer
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.shortcuts import get_object_or_404
 
 def workflow_list() :
     workflows  = WorkFlow.objects.select_related("created_by").all()
     serializer = workflow_serializer(
         workflows , many=True
     ) 
+    return Response(serializer.data , status=200)
+
+def get_workflow_by_id(workflow_id) :
+    workflow = get_object_or_404(WorkFlow , id=workflow_id)
+    serializer = workflow_serializer(workflow)
     return Response(serializer.data , status=200)
