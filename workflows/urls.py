346eeee8962f3184434f1356c94255203dff39d1
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-21 20:52:56 +0100

    add workflow delete path

--- workflows/urls.py
+++ workflows/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('workflows-lists/', views.workflow_api_list.as_view()),
     path('workflow/<int:workflow_id>/workflow-detail', views.workflow_by_ID.as_view()),
+    path('workflow/<int:workflow_id>/workflow-delete', views.delete_workflow_view.as_view()),
+
 ] 
