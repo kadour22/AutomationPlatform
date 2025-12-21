79dc80727e52dd7c9652210bb73633d9e8cb600e
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-21 20:52:03 +0100

    add workflow detail path

--- workflows/urls.py
+++ workflows/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('workflows-lists/', views.workflow_api_list.as_view()),
+    path('workflow/<int:workflow_id>/workflow-detail', views.workflow_by_ID.as_view()),
 ] 
