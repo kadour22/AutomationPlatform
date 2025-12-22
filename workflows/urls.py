dd2d76c2b10d289d5790de2ce168221b30b0a702
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-22 12:01:47 +0100

    add create workflow step url

--- workflows/urls.py
+++ workflows/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    # workflows urls
     path('workflows-lists/', views.workflow_api_list.as_view()),
     path('workflow/<int:workflow_id>/workflow-detail', views.workflow_by_ID.as_view()),
     path('workflow/<int:workflow_id>/workflow-delete', views.delete_workflow_view.as_view()),
-
+    # workflows steps urls
+    path('workflow-step-create/' , views.create_workflow_steps_view.as_view())
 ] 
