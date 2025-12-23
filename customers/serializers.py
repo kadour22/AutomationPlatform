75a6eabbea44ab1625589fa149250f9ae30fe911
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-23 20:28:46 +0100

    add validate && create serializer methods for customer

--- customers/serializers.py
+++ customers/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers 
 from .models import User
+from django.core.exceptions import ValidationError
+
 
 class customer_serializer(serializers.ModelSerializer) :
     class Meta :
         model = User
         fields= "__all__"
+    
+    def validate(self,attrs) :
+        if attrs["role"] == "" :
+            raise ValidationError(
+                "Role field is required"
+            )
+        return attrs
+    
+    def create(self, validated_data):
+       password = validated_data.pop('password')
+       user = User.objects.create_user(**validated_data, password=password)
+       return user
