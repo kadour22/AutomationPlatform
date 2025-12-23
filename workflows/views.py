8977e83b1757a001e5f6b6fc99a717938e6cbfbc
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-23 20:41:41 +0100

    add owner permission to views

--- workflows/views.py
+++ workflows/views.py
@@ -3,25 +3,29 @@ from rest_framework.response import Response
 from rest_framework import status
 from workflows.models import WorkFlowStep
 from workflows.serializers import workflow_steps_serializer
-
 from workflows.services.workflow_services import (
     workflow_list , get_workflow_by_id , delete_workflow
 )
 
 from workflows.services.workflow_steps import create_workflow_steps
 
+from .permmsions import IsOwnerPermission
 class workflow_api_list(APIView) :
+    permission_class = [IsOwnerPermission]
     def get(self, request) :
         return workflow_list()
 
 class workflow_by_ID(APIView) :
+    permission_class = [IsOwnerPermission]
     def get(self, request, workflow_id) :
         return get_workflow_by_id(workflow_id=workflow_id)
     
 class delete_workflow_view(APIView) :
+    permission_class = [IsOwnerPermission]
     def delete(self, request , workflow_id) :
         return delete_workflow(workflow_id=workflow_id)
     
 class create_workflow_steps_view(APIView) :
+    permission_class = [IsOwnerPermission]
     def post(self, request) :
         return create_workflow_steps(data=request.data)
