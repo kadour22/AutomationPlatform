38cb47fa1e390c2e6edca87b95f8d9a972ad6ddf
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-23 16:51:07 +0100

    add all sensitive data and keys to .env file

--- AutomationPlatform/settings.py
+++ AutomationPlatform/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'AutomationPlatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'automation',
-        'USER':'postgres',
-        'PASSWORD':'zaza',
-        'HOST':'localhost',
-        'PORT':5432
+        'ENGINE': os.getenv("ENGINE"),
+        'NAME': os.getenv("NAME"),
+        'USER':os.getenv("USER"),
+        'PASSWORD':os.getenv("PASSWORD"),
+        'HOST':os.getenv("HOST"),
+        'PORT':os.getenv("PORT")
     }
 }
 
@@ -127,10 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-BREVO_API_KEY = os.get_env("BREVO_API_KEY")
-EMAIL_BACKEND = os.get_env("EMAIL_BACKEND")
-EMAIL_HOST = os.get_env("EMAIL_HOST")
-EMAIL_PORT = os.get_env("EMAIL_PORT")
-EMAIL_USE_TLS = os.get_env("EMAIL_USE_TLS")
-EMAIL_HOST_USER = os.get_env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.get_env("EMAIL_HOST_PASSWORD")
+BREVO_API_KEY = os.getenv("BREVO_API_KEY")
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
