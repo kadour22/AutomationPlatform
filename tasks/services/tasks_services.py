a26ddafbf145a8b5db3abcd387e58955050bd2b1
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-24 15:44:25 +0100

    create delete tasks service

--- tasks/services/tasks_services.py
+++ tasks/services/tasks_services.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
-
+from django.shortcuts import get_object_or_404
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 
@@ -12,3 +12,9 @@ def tasks_list_service(request) :
     )
     return Response(serializer.data , status=200)
 
+def delete_task_service(request,task_id) :
+    task = get_object_or_404(Task , id = task_id)
+    task.delete()
+    return Response({
+        "message" : f"task : {task.title} deleted "
+    },status=200)
