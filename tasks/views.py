99acd3647cfbdb4a5b2dc18570225390e4c3ef2b
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-24 17:41:31 +0100

    create all tasks views logic

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,15 @@
 from tasks.services.tasks_services import (
-    tasks_list_service , delete_task_service
+    tasks_list_service , delete_task_service , create_task_service
 )
-
 from rest_framework.views import APIView
 
-
-
 class tasks_view_logic(APIView) :
-    def get(self, request,task_id=None) :
-        return 
+    
+    def get(self, request) :
+        return tasks_list_service(request=request)
+    
+    def post(self,request) :
+        return create_task_service(user=request.user , data=request.data)
+    
+    def delete(self,request,task_id) :
+        return delete_task_service(task_id=task_id)
