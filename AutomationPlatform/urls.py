8cf0e73ce0a4c2fb0124e5fd0de87509dbbe0776
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-24 17:45:43 +0100

    configuer tasks routes

--- AutomationPlatform/urls.py
+++ AutomationPlatform/urls.py
@@ -2,14 +2,16 @@
 from django.contrib import admin
 from django.urls import path , include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('workflows/' , include('workflows.urls')),
     path('executions/' , include('exections.urls')),
     path('customers/' , include('customers.urls')),
+    path('tasks/' , include('tasks.urls')),
     
+
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+
+urlpatterns = [
+    path('tasks/', views.tasks_view_logic.as_view(), name='tasks-list-create'),
+    path('tasks/<int:task_id>/', views.tasks_view_logic.as_view(), name='tasks-delete'),
+]
