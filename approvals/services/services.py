6e87456eb7fa3937845b532cd00e6938b385a453
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-25 23:07:29 +0100

    fixe approval creation service

--- approvals/services/services.py
+++ approvals/services/services.py
@@ -2,8 +2,10 @@ from approvals.models import Approval
 from approvals.serializers import ApprovalSerializer
 
 from rest_framework.response import Response
+
 def create_approval(data , requester):
     serializer = ApprovalSerializer(data=data)
     if serializer.is_valid(raise_exception=True):
         serializer.save(requester=requester)
         return Response(serializer.data,status=201)
+    return Response(serializer.errors, status=400)
