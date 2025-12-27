9fd0f17dff416a5f7acf699a4ae6bc058edba119
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-27 23:15:22 +0100

    delete workflow engine file

--- workflows/permmsions.py
+++ workflows/permmsions.py
@@ -11,7 +11,6 @@ def ManagerPermission(BasePermission):
     def has_object_permission(self, request, obj, view) :
         return request.user.role == "manager"
     
-
 def IsAdmin(BasePermission):
     def has_object_permission(self, request, obj, view) :
         return request.user.role == "admin"
