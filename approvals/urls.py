57bff9a4ff71b240e5ca66e1a2ee072085c6546f
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-12-27 23:09:44 +0100

    update urls

--- approvals/urls.py
+++ approvals/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("approvals/", views.approvals_list, name="approvals_list"),
     path("approvals/<int:approval_id>/", views.approval_detail, name="approval_detail"),
+    path('approvals/<int:approval_id>/delete/', views.approval_delete, name="approval_delete"),
+    # path('approvals/create/', views.create_approval_view, name="create_approval"),
 ]
